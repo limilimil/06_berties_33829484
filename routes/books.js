935a411c631f01ae8461a3c94bd9b6db7d79936f
lcann <liamcannon93@gmail.com>
2025-11-14 12:36:12 +0000

    Implement book listing and addition features; Update the home page menu

--- routes/books.js
+++ routes/books.js
@@ -2,13 +2,13 @@
 const express = require("express")
 const router = express.Router()
 
-router.get('/search',function(req, res, next){
-    res.render("search.ejs")
+router.get('/search', function(req, res, next){
+    res.render("search.ejs");
 });
 
 router.get('/search-result', function (req, res, next) {
     //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    res.send("You searched for: " + req.query.keyword);
 });
 
 router.get('/list', function(req, res, next) {
@@ -18,7 +18,25 @@ router.get('/list', function(req, res, next) {
         if (err) {
             next(err)
         }
-        res.send(result)
+        res.render("list.ejs", {availableBooks:result});
+    });
+});
+
+router.get('/addbook', function(req, res, next){
+    res.render("addbook.ejs");
+});
+
+router.post('/bookadded', function (req, res, next) {
+    // saving data in database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+    // execute sql query
+    let newrecord = [req.body.name, req.body.price]
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        else
+            res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price);
     });
 });
 
