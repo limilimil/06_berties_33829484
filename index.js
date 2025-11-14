449063a2e33f9d155157d3ead9bf23d99f963a0b
lcann <liamcannon93@gmail.com>
2025-11-14 13:19:12 +0000

    Refactor code formatting for consistency and clarity; Updatr code with comments

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Import express and ejs
-var express = require ('express');
-var ejs = require('ejs');
+const express = require ('express');
+const ejs = require('ejs');
 const path = require('path');
-var mysql = require('mysql2');
+const mysql = require('mysql2');
 
 // Create the express application object
-const app = express()
-const port = 8000
+const app = express();
+const port = 8000;
 
 // Tell Express that we want to use EJS as the templating engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // Set up the body parser 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // Set up public folder (for css and static js)
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Define our application-specific data
-app.locals.shopData = {shopName: "Bertie's Books"}
+app.locals.shopData = {shopName: "Bertie's Books"};
 
 // Define the database connection pool
 const db = mysql.createPool({
@@ -34,16 +34,16 @@ global.db = db;
 
 
 // Load the route handlers
-const mainRoutes = require("./routes/main")
-app.use('/', mainRoutes)
+const mainRoutes = require('./routes/main');
+app.use('/', mainRoutes);
 
 // Load the route handlers for /users
-const usersRoutes = require('./routes/users')
-app.use('/users', usersRoutes)
+const usersRoutes = require('./routes/users');
+app.use('/users', usersRoutes);
 
 // Load the route handlers for /books
-const booksRoutes = require('./routes/books')
-app.use('/books', booksRoutes)
+const booksRoutes = require('./routes/books');
+app.use('/books', booksRoutes);
 
 // Start the web app listening
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,10 @@
 // Create a new router
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/search', function(req, res, next){
-    res.render("search.ejs");
+// Handle books routes
+router.get('/search', function(req, res, next) {
+    res.render('search.ejs');
 });
 
 router.get('/search-result', function (req, res, next) {
@@ -16,30 +17,31 @@ router.get('/list', function(req, res, next) {
     // execute sql query
     db.query(sqlquery, (err, result) => {
         if (err) {
-            next(err)
+            next(err);
         }
-        res.render("list.ejs", {availableBooks:result});
+        res.render('list.ejs', {availableBooks:result});
     });
 });
 
-router.get('/addbook', function(req, res, next){
-    res.render("addbook.ejs");
+router.get('/addbook', function(req, res, next) {
+    res.render('addbook.ejs');
 });
 
 router.post('/bookadded', function (req, res, next) {
     // saving data in database
-    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
     // execute sql query
     let newrecord = [req.body.name, req.body.price]
     db.query(sqlquery, newrecord, (err, result) => {
         if (err) {
-            next(err)
+            next(err);
         }
         else
+            // Message sent to user
             res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price);
     });
 });
 
 
 // Export the router object so index.js can access it
-module.exports = router
+module.exports = router;
