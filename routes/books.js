b131bfdb1ad3b51187349c410cba305fa91a2a19
lcann <liamcannon93@gmail.com>
2025-11-29 18:09:05 +0000

    Add access control to books routes and update Readme

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,14 @@
 const express = require("express");
 const router = express.Router();
 
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId ) {
+      res.redirect('/users/login'); // redirect to the login page
+    } else { 
+        next (); // move to the next middleware function
+    } 
+}
+
 // Handle books routes
 router.get('/search', function(req, res, next) {
     res.render('search.ejs');
@@ -30,11 +38,11 @@ router.get('/list', function(req, res, next) {
     });
 });
 
-router.get('/addbook', function(req, res, next) {
+router.get('/addbook', redirectLogin, function(req, res, next) {
     res.render('addbook.ejs');
 });
 
-router.post('/bookadded', function (req, res, next) {
+router.post('/bookadded', redirectLogin, function (req, res, next) {
     // saving data in database
     let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
     // execute sql query
